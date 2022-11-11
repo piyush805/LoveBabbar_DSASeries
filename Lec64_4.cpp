81c277015a41d1ecc4bc54eefd3acca7d671c543
Piyush Tiwari <pshtiwari0@gmail.com>
2022-11-11 22:45:29 +0530

    Q: Top View of a binary tree

---
+++ Lec64_4.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+#include <map>
+#include <queue>
+using namespace std;
+//================ Top View of binary Tree ==================
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+    node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void levelOrderTraversal_line(node *root)
+{
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL); // 1st level completed at root itself
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+        // temp = root, pop root, push root->left, root right
+        // temp = NULL, pop NULL, push null,(prvious level over)
+        // temp = root->left, pop root->left, push its left and right
+        // temp = root->right, pop root->right push its left and right
+        // temp = NULL, pop NULL, push NULL(previous level over)
+        // continues...
+
+        if (temp == NULL) //
+        {
+            cout << endl;
+            if (!q.empty()) // if there are still elements then these are that of next level->so now push a separator again
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            // print data and push its left and right
+            cout << temp->data << " ";
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+void buildFromLevelOrder(node *&root)
+{
+    queue<node *> q;
+    cout << "Enter root data: " << endl;
+    int data;
+    cin >> data;
+    root = new node(data);
+    q.push(root);
+
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+        // Take and attach left node
+        // cout << "Enter the left node for " << temp->data << ":" << endl;
+        int leftData;
+        cin >> leftData;
+        if (leftData != -1)
+        {
+            temp->left = new node(leftData);
+            q.push(temp->left);
+        }
+        // take and attach right node
+        // cout << "Enter the right node for " << temp->data << ":"<<endl;
+        int rightData;
+        cin >> rightData;
+        if (rightData != -1)
+        {
+            temp->right = new node(rightData);
+            q.push(temp->right);
+        }
+    }
+}
+
+vector<int> topView(node *root)
+{
+    vector<int> ans;
+    if (root == NULL)
+        return ans;
+
+    map<int, int> topNode;      // [hd]-->[nodeData]
+    queue<pair<node *, int>> q; //<node*, hd>
+
+    q.push(make_pair(root, 0));
+
+    while (!q.empty())
+    {
+        pair<node *, int> temp = q.front();
+        q.pop();
+
+        node *frontNode = temp.first; // node*
+        int hd = temp.second;         // hd
+
+        if (topNode.find(hd) == topNode.end())
+        {
+            // if entry for that horizontal distance not available
+            topNode[hd] = frontNode->data; // make entry
+        }
+        // push in left subtree
+        if (frontNode->left)
+            q.push(make_pair(frontNode->left, hd - 1));
+        // push in right subtree
+        if (frontNode->right)
+            q.push(make_pair(frontNode->right, hd + 1));
+    }
+    for (auto i : topNode)       // for each [hd]->[nodedata]
+        ans.push_back(i.second); // push in [nodeData]
+
+    return ans;
+}
+
+int main()
+{
+    node *root = NULL;
+    // root = buildTree(root);
+    buildFromLevelOrder(root);
+    // i/p: 1 2 3 4 5 6 7 -1 -1 -1 -1 -1 8 -1 9 -1 -1 -1 -1
+    /*
+            1
+           / \
+          2   3
+        / \  / \
+       4   56   7
+            \    \
+             8    9    
+    */
+    // o/p: 4 2 1 3 7 9
+    cout << endl;
+    cout << "Level order traversal:" << endl;
+    levelOrderTraversal_line(root);
+    cout << endl;
+
+    cout << "Top view L->R: ";
+    vector<int> top = topView(root);
+    for (int i = 0; i < top.size(); i++)
+    {
+        cout << top[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
